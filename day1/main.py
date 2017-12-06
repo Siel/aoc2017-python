b76944a38a4c195b9c4f3ac02641862fcc58907c
Julian D. Otalvaro <juliandavid347@gmail.com>
2017-12-06 16:07:44 -0500

    day1.1 solved

--- day1/main.py
+++ day1/main.py
@@ -1,1 +1,10 @@
-
+file=open("data/input_data.txt")
+line = file.read()
+if line[0] == line[len(line)-1]:
+    sum = int(line[0])
+else:
+    sum = 0
+for i in range(len(line)-1):
+    if line[i] == line[i+1]:
+        sum = sum + int(line[i])
+print(sum)
